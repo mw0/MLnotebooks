7613397ace13abf939fc229b5e4f368ee9c23239
Mark Wilber <markwilber@alumni.brown.edu>
2020-04-18 18:39:39 -0700

    Latests tweaks, while running tests of ALS.

--- Implicit/runQuasiGrid.sh
+++ Implicit/runQuasiGrid.sh
@@ -1,7 +1,23 @@
-python/runImplicit.py --factors 64 --useGPU True --lambda 0.01 --iterations 40 --k 20 --numThreads 6
-sleep(10)
-python/runImplicit.py --factors 64 --useGPU True --lambda 0.03 --iterations 40 --k 20 --numThreads 6
-sleep(10)
-python/runImplicit.py --factors 64 --useGPU True --lambda 0.09 --iterations 40 --k 20 --numThreads 6
-sleep(10)
-python/runImplicit.py --factors 64 --useGPU True --lambda 0.27 --iterations 40 --k 20 --numThreads 6
+CUDA_VISIBLE_DEVICES=1 python/runImplicit.py --factors 64 --useGPU True --lambda 0.01 --iterations 40 --k 20 --numThreads 3 &
+
+CUDA_VISIBLE_DEVICES=0 python/runImplicit.py --factors 64 --useGPU True --lambda 0.03 --iterations 40 --k 20 --numThreads 3 &
+
+CUDA_VISIBLE_DEVICES=1 python/runImplicit.py --factors 64 --useGPU True --lambda 0.09 --iterations 40 --k 20 --numThreads 3 &
+
+CUDA_VISIBLE_DEVICES=0 python/runImplicit.py --factors 64 --useGPU True --lambda 0.27 --iterations 40 --k 20 --numThreads 3 &
+
+CUDA_VISIBLE_DEVICES=1 python/runImplicit.py --factors 128 --useGPU True --lambda 0.01 --iterations 40 --k 20 --numThreads 3 &
+
+CUDA_VISIBLE_DEVICES=0 python/runImplicit.py --factors 128 --useGPU True --lambda 0.03 --iterations 40 --k 20 --numThreads 3 &
+
+CUDA_VISIBLE_DEVICES=1 python/runImplicit.py --factors 128 --useGPU True --lambda 0.09 --iterations 40 --k 20 --numThreads 3 &
+
+CUDA_VISIBLE_DEVICES=0 python/runImplicit.py --factors 128 --useGPU True --lambda 0.27 --iterations 40 --k 20 --numThreads 3 &
+
+CUDA_VISIBLE_DEVICES=1 python/runImplicit.py --factors 128 --useGPU True --lambda 0.81 --iterations 40 --k 20 --numThreads 6 &
+
+CUDA_VISIBLE_DEVICES=0 python/runImplicit.py --factors 128 --useGPU True --lambda 2.43 --iterations 40 --k 20 --numThreads 6 &
+
+CUDA_VISIBLE_DEVICES=1 python/runImplicit.py --factors 128 --useGPU True --lambda 7.29 --iterations 40 --k 20 --numThreads 6 &
+
+CUDA_VISIBLE_DEVICES=0 python/runImplicit.py --factors 128 --useGPU True --lambda 21.87 --iterations 40 --k 20 --numThreads 6 &
