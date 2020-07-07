bc325fd7464801418f7c4f5bb11d771a849747cc
MarkWilber <mwilberssl@gmail.com>
2020-07-06 17:29:55 -0700

    With success of initializing with device=0, increasing summarization defaults.

--- HuggingFace/python/streamlitSummarizer.py
+++ HuggingFace/python/streamlitSummarizer.py
@@ -112,10 +112,10 @@ st.sidebar.info(
 )
 
 st.sidebar.header("Set summarization output range (words)")
-minLength = st.sidebar.slider("min. word count", 25, 250, 165)
-maxLength = st.sidebar.slider("max. word count", 45, 310, 190)
+minLength = st.sidebar.slider("min. word count", 25, 250, 185)
+maxLength = st.sidebar.slider("max. word count", 45, 310, 225)
 st.sidebar.header("Article truncation size (words)")
-truncateWords = st.sidebar.slider("truncate size", 300, 720, 500)
+truncateWords = st.sidebar.slider("truncate size", 300, 720, 720)
 
 st.sidebar.title("Top 5 New York Times world news articles")
 
@@ -162,20 +162,20 @@ st.write(userText)
 t11 = perf_counter()
 Δt10 = t11 - t10
 
-print(f"Δt to fetch top 5 article metadatums: {Δt01:4.1f}s")
-print(f"Δt to generate sidebar dropdown: {Δt23:4.1f}s")
-print(f"Δt to fetch article: {Δt45:4.1f}s")
-print(f"Δt to soupify article: {Δt67:4.1f}s")
-print(f"Δt to summarize article: {Δt89:4.1f}s")
-print(f"Δt to write article: {Δt10:4.1f}s")
+print(f"Δt to fetch top 5 article meta: {Δt01:5.2f}s")
+print(f"Δt to generate sidebar dropdown: {Δt23:5.2f}s")
+print(f"Δt to fetch article: {Δt45:5.2f}s")
+print(f"Δt to soupify article: {Δt67:5.2f}s")
+print(f"Δt to summarize article: {Δt89:5.2f}s")
+print(f"Δt to write article: {Δt10:5.2f}s")
 
 if not st.sidebar.button('Hide profiling information'):
     st.sidebar.header('Profiling information')
-    sbInfoStr = (f"* initialize summarizer Δt: {Δt01:4.1f}s\n"
-                 f"* fetch top 5 article metada Δt: {Δt23:4.1f}s\n"
-                 f"* fetch selected article Δt: {Δt45:4.1f}s\n"
-                 f"* soupify article Δt: {Δt67:4.1f}s\n"
-                 f"* summarize article Δt: {Δt89:4.1f}s")
+    sbInfoStr = (f"* initialize summarizer: {Δt01:5.2f}s\n"
+                 f"* fetch top 5 article metadata: {Δt23:5.2f}s\n"
+                 f"* fetch selected article: {Δt45:5.2f}s\n"
+                 f"* soupify article: {Δt67:5.2f}s\n"
+                 f"* summarize article: {Δt89:5.2f}s")
     if cudaDetected:
         sbInfoStr += "\n"
         for i in range(cudaDeviceCt):
@@ -183,8 +183,8 @@ if not st.sidebar.button('Hide profiling information'):
             cached = round(torch.cuda.memory_cached(i)/1024**3,1)
             sbInfoStr += (f"\n\ncuda device[{i}]:"
                           # f" {torch.cuda.get_device_name(i)}"
-                          f"\n* Allocated memory: {allocated} GB\n"
-                          f"* Cached memory: {cached} GB")
+                          f"\n* Allocated memory: {allocated:4.2f} GB\n"
+                          f"* Cached memory: {cached:4.2f} GB")
     print(sbInfoStr)
     st.sidebar.info(sbInfoStr)
 
