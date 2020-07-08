79077c565d3a23a9a267dfcc503ea88ddcc4c542
MarkWilber <mwilberssl@gmail.com>
2020-07-07 19:14:42 -0700

    Now should reset cache for fetchTop5TitlesURL() every 3 minutes, so top 5 story rankings are fresh-ish.

--- HuggingFace/python/streamlitSummarizer.py
+++ HuggingFace/python/streamlitSummarizer.py
@@ -27,7 +27,7 @@ def initializeSummarizer():
     return pipeline("summarization", device=0)
 
 
-# @st.cache(suppress_st_warning=True)
+@st.cache(ttl=180)		# clear cache every 3 minutes
 def fetchTop5TitlesURLs():
     top5WorldStories = nyt.top_stories(section="world")[:5]
 
